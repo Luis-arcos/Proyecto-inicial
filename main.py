61c5ecfae46c99949045a6f07372a7e17d557573
ArCas <xz.laac95@gmail.com>
2026-03-12 13:16:46 -0600

    cambio desde termux

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 print("hola mundo de la programacion")
 nombre = input("como te llamas?:  ")
 print ("bienvenido", nombre)
+
+//cambio de dispositivo
+
+print ("escribiendo desde termux android ahora")
