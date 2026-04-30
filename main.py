64a5629f0d9d8922450a68a2d515716c54bd2d4d
ArCas <xz.laac95@gmail.com>
2026-04-30 12:45:01 -0600

    cambio

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
+#pequeña bienvenida
 print("hola mundo de la programacion")
 nombre = input("como te llamas?:  ")
 print ("bienvenido", nombre)
 
-//cambio de dispositivo
+#cambio de dispositivo
+print ("hagamos una suma")
+while True:
+    try:
+        a = float(input ("ingresa el primer numero: "))
+        b = float(input ("ingresa el segundo numero: "))
+        resultado = a + b 
+        print ("resultado", resultado)
+        break
 
-print ("escribiendo desde termux android ahora")
+    except:
+        print ("¡¡¡ERROR!!! debe ingresar solo numeros")
+
+#eval(): ejecuta texto como codigo 
+#usar eval con precaucion mayormente usar solo para pruebas 
+
+#hacer suma con multiplicacion 
+print ("ingresa dos numeros para sumar y uno para multiplicar")
+while True:
+    try:
+        numero1 = float(input("ingresa el primer numero: "))
+        numero2 = float(input("ingresa el segundo numero: "))
+        resultados = numero1+numero2 
+
+        print("el resultado es: ", resultados)
+        multi = float(input("ingresa numero para multiplicar: "))
+        print ("el resultado es: ", resultados * multi)
+        break
+
+    except:
+        print ("ingrese solo numeros")
