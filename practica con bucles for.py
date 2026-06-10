3c8abe9ddeb85dc6bde56580f7a60a4dd48b7c5d
ArCas <xz.laac95@gmail.com>
2026-06-09 22:47:41 -0600

    practica con bucles for

--- practica con bucles for.py
+++ practica con bucles for.py
@@ -1,5 +1,8 @@
+#for es el ciclo (haz esto varias veces pero siguiendo una secuencia)
+# in es el conector
+#range() es el generador de numeros, crea la secuencia que for va a recorrer 
 for i in range (10):
     print ("hola")
-
+#0 es de donde inicia a contar, 31 hasta donde y 10 de cuanto en cuanto debe contar
 for x in range (0,31,10):
     print (x)
